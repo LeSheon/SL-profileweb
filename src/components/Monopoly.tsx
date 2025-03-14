f40999be4e9aab37bab10ae06ff4c8938526e950
Sheon Lee <dlelfo7@gmail.com>
2025-03-14 14:31:50 -0400

    Add foundation of monopoly system

--- src/components/Monopoly.tsx
+++ src/components/Monopoly.tsx
@@ -1,13 +1,48 @@
 import "./monopoly.scss";
-import React from "react";
+import { useState } from "react";
 
-const Monopoly: React.FC = () => {
-  return (
-    <div>
-      <h1>Monopoly Game</h1>
-      <p>Welcome to the simple Monopoly game!</p>
-    </div>
-  );
+export default function Monopoly() {
+    const [position, setPosition] = useState(0);
+
+    function rollDice() {
+        const dice = Math.floor(Math.random()*2)+1;
+        // most likely prev holds same value as position.
+        // but, there is possibility where position is not up-to-date so use prev.
+        setPosition((prev) => (prev + dice) %4);
+    }
+
+    return(
+        <>
+        <section className="monopoly">
+            <h2>Monopoly Board in Test</h2>
+            <div className="monopoly__board">
+
+            </div>
+            <button className="monopoly__dice-roll" onClick={() => rollDice()}>
+                Roll Dice
+            </button>
+        </section>
+        </>
+    );
 };
 
-export default Monopoly;
+//   return (
+//     <div className="monopoly-board">
+//       <h2>Monopoly Game</h2>
+//       <div className="board">
+//         {/* Create a 2x2 grid */}
+//         {[...Array(4)].map((_, index) => (
+//           <div
+//             key={index}
+//             className={`board-space ${index === position ? "active" : ""}`}
+//           >
+//             {index + 1}
+//           </div>
+//         ))}
+//       </div>
+//       <button onClick={rollDice}>Roll Dice</button>
+//     </div>
+//   );
+// };
+
+// export default Monopoly;
